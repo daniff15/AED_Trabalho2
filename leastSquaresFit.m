45b6895900710d8c68f85044dbbcce00c28ab5fb
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-01-14 01:39:05 +0000

    Add files via upload

--- leastSquaresFit.m
+++ leastSquaresFit.m
@@ -14,20 +14,19 @@ for o = 1:(length(idx)-1)
     
     i = idx(o);
     seguinte = idx(o + 1);
-    x = avg_time(i:seguinte,1);
-    y = avg_time(i:seguinte , 2);
-    X = [ x.^2, x, 0*x+1 ];
-    w = pinv(X)*y;
+    x = avg_time(i:seguinte,1); % extract the first column
+    y = avg_time(i:seguinte , 2);% extract the second column
+    X = [ x.^2, x, 0*x+1 ];% build the X matrix
+    w = pinv(X)*y;% optimal solution (could also be written as w = X \ y;)
     e = y-X*w;
     format long;
-    norm(e);
+    norm(e)
     figure;
-    plot(x,y,'.r',x,X*w,'og');
+    plot(x,y,'.r',x,X*w,'og');% plot the original data and its best least squares approximation
     title(strs(o));
     xlabel("n");
     ylabel("Tempo(s)");
     legend('Tempos médios','X*w')
-    
   
 end
 
