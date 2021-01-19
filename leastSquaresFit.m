8e8ef770a7c996706178dcbe216d48bf5032c516
Dani Figueiredo <73801594+daniff15@users.noreply.github.com>
2021-01-19 18:29:28 +0000

    Add files via upload

--- leastSquaresFit.m
+++ leastSquaresFit.m
@@ -9,14 +9,18 @@ sizeAvg = size(avg_time);
 idx(end+1) = sizeAvg(1);  
 strs = ["Bubble Sort" , "Shaker Sort", "Insertion Sort" , "Shell Sort" , "Quick Sort" , "Merge Sort" , "Heap Sort" , "Rank Sort", "Selection Sort"];
 
-
 for o = 1:(length(idx)-1)
-    
     i = idx(o);
     seguinte = idx(o + 1);
     x = avg_time(i:seguinte,1); % extract the first column
     y = avg_time(i:seguinte , 2);% extract the second column
-    X = [ x.^2, x, 0*x+1 ];% build the X matrix
+    
+    if(o < 4 || o > 7)
+        X = [ x.^2, x, 0*x+1 ];% build the X matrix
+    else
+        X = [ x.*log(x), x, 0*x+1 ];% build the X matrix
+    end
+    
     w = pinv(X)*y;% optimal solution (could also be written as w = X \ y;)
     e = y-X*w;
     format long;
